4e438ea6cdeab577015ed9a75dfd16c1e51cbbaa
punkkk <victor@kozlov.io>
2019-08-19 09:48:54 +0300

    initial commit (splash screen + menu background)

---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class GameLoader : MonoBehaviour
+{
+    [SerializeField] private float loadingDelay = 3f;
+    private void Start()
+    {
+        StartCoroutine(loadMainMenu());
+    }
+
+    IEnumerator loadMainMenu()
+    {
+        yield return new WaitForSeconds(loadingDelay);
+        
+        FindObjectOfType<SceneLoader>().MainMenu();
+    }
+}
