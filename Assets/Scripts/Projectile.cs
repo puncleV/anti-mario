938d7b16a9345eba88a6f0723b4dbd186943f058
punkkk <victor@kozlov.io>
2019-10-25 09:07:27 +0300

    add projectile for a defender

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private int damage = 100;
+    [SerializeField] private float speed = 5f;
+
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime * Vector2.right);
+    }
+}
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -2,8 +2,12 @@
 
 public class Shooter : MonoBehaviour
 {
+    [SerializeField] private GameObject projectile;
     public void Fire()
     {
-        return;
+        Instantiate(projectile, new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z),
+            Quaternion.identity);
+        
+
     }
 }
