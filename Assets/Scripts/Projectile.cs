29e29ac3efd91c58aadd6888c5069d4939092779
punkkk <victor@kozlov.io>
2019-10-31 09:33:08 +0300

    get rid of damage dealer, move logic into projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,11 +3,24 @@
 public class Projectile : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private int damage = 100;
 
     void Update()
     {
         transform.Translate(speed * Time.deltaTime * Vector2.right);
     }
     
-   
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        Health health = collider.gameObject.GetComponent<Health>();
+
+        health.DealDamage(damage);
+
+        if (health.Hp <= 0)
+        {
+            Destroy(collider.gameObject);    
+        }
+        
+        Destroy(gameObject);
+    }
 }
