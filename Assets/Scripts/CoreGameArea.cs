5f21ce0134a7b4419ab36c3f31aee61d075502b2
punkkk <victor@kozlov.io>
2019-11-06 19:52:45 +0300

    spawn defenders

---
+++ Assets/Scripts/CoreGameArea.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CoreGameArea : MonoBehaviour
+{
+    [SerializeField] private GameObject defender;
+    private void OnMouseDown()
+    {
+        SpawnDefender();
+    }
+
+    private void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity);
+    }
+}
