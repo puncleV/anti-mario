c7b08cd0a084464c7982f965d1c4ba1f4c603867
punkkk <victor@kozlov.io>
2019-11-07 18:52:42 +0300

    add defenders buttons

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        var sprite = GetComponent<SpriteRenderer>();
+        
+        sprite.color = Color.white;
+    }
+}
