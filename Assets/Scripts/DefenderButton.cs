68c68ada120b2bb4184ea247797486d137e87c04
punkkk <victor@kozlov.io>
2019-11-07 18:57:36 +0300

    return color to a defender button after mouse btn release

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 
 public class DefenderButton : MonoBehaviour
 {
+    private Color startColor;
+    private void Start()
+    {
+        startColor = GetComponent<SpriteRenderer>().color;
+    }
+
     private void OnMouseDown()
     {
         var sprite = GetComponent<SpriteRenderer>();
         
         sprite.color = Color.white;
+    }
+
+    private void OnMouseUp()
+    {
+        var sprite = GetComponent<SpriteRenderer>();
+        
+        sprite.color = startColor;
     }
 }
