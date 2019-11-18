c3fbccaf4fdf4738739e43d450ae73cc030aa298
punkkk <victor@kozlov.io>
2019-11-18 09:51:51 +0300

    add stars script and display

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] private int stars = 100;
+    private Text starText;
+    void Start()
+    {
+        starText = GetComponent<Text>();
+        UpdateDisplay();
+    }
+
+    public void AddStars(int count)
+    {
+        stars += count;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int count)
+    {
+        if (stars - count < 0) return;
+        
+        stars -= count;
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        starText.text = stars.ToString();
+    }
+}
