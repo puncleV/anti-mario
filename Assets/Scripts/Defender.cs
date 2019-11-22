d20a2be7a4279d44423fb0312019cc8d20b37128
punkkk <victor@kozlov.io>
2019-11-22 09:54:57 +0300

    add stars by generator

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] private int cost = 100;
+
+    public void AddStars(int amount)
+    {
+        var starDisplay = FindObjectOfType<StarDisplay>();
+        
+        starDisplay.AddStars(amount);
+    }
 }
