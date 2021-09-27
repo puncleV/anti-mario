45d26114c03fa2f167dfcfa610c52f2649430db4
Viktor <punkrus18@gmail.com>
2021-09-27 20:42:26 +0300

    add: attacker detection

--- Assets/Scripts/AttackerSpawner.cs
+++ Assets/Scripts/AttackerSpawner.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+
 public class AttackerSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject enemyPrefab;
-    
+    [FormerlySerializedAs("enemyPrefab")] [SerializeField] private Attacker attackerPrefab;
     [SerializeField] private Boolean on = true;
     
     [SerializeField] private float minSpawnRate = 1f;
     [SerializeField] private float maxSpawnRate = 5f;
+
     
     private Coroutine spawnCoroutine = null;
     IEnumerator Start()
@@ -22,7 +24,10 @@ public class AttackerSpawner : MonoBehaviour
 
     IEnumerator spawn()
     {
-        Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        Attacker attacker = Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+
+        attacker.transform.parent = transform;
+        
         yield return new WaitForSeconds(UnityEngine.Random.Range(minSpawnRate, maxSpawnRate));
     }
 }
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,9 +1,48 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
     [SerializeField] private GameObject projectile;
     [SerializeField] private GameObject gun;
+
+    private AttackerSpawner lineSpawner;
+    
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("pew pew");
+        }
+        else
+        {
+            Debug.Log("no pew pew");
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        return lineSpawner.transform.childCount <= 0;
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] attackerSpawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in attackerSpawners)
+        {
+            if (Math.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon)
+            {
+                lineSpawner = spawner;
+            }
+        }
+    }
+    
     public void Fire()
     {
         Instantiate(projectile, gun.transform.position,
