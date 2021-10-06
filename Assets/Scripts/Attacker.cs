fc47f37784f360a30a7dfa6dbc2608f49b6d0771
Viktor <punkrus18@gmail.com>
2021-10-06 20:02:50 +0300

    add: attackers now can attack

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,13 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
     [Range(0f, 5f)][SerializeField] private float speed = 1f; 
-    [Range(100, 300)][SerializeField] private int hp = 100; 
+    [Range(100, 300)][SerializeField] private int hp = 100;
+
+    private Defender currentTarget;
+    
+    private static readonly int IsAttacking = Animator.StringToHash("isAttacking");
+
     void Update()
     {
         transform.Translate(speed * Time.deltaTime * Vector2.left);
+    }
+
+    void SetMovementSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+
+    public void Attack(Defender target)
+    {
+        GetComponent<Animator>().SetBool(IsAttacking, true);
+
+        currentTarget = target;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject collidedWith = other.gameObject;
+
+        Defender defender = collidedWith.GetComponent<Defender>();
+
+        if (defender != null)
+        {
+            Attack(defender);
+        }
     }
 }
